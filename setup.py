5fc54fceca1c6f10165d78a3dad92ea83791b040
shaddin47 <74891079+shaddin47@users.noreply.github.com>
2022-09-01 11:42:40 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from my_pip_package import __version__
 
@@ -10,5 +10,5 @@ setup(
     author='Scott Haddin',
     author_email='scotth@cqg.com',
 
-    py_modules=['my_pip_package'],
+    packages=find_packages()
 )
