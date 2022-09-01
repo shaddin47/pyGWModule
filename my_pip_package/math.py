14010751b8f8de965e09d6abbbc59fb7b69e5484
shaddin47 <74891079+shaddin47@users.noreply.github.com>
2022-09-01 13:12:39 +1000

    Update math.py

--- my_pip_package/math.py
+++ my_pip_package/math.py
@@ -10,3 +10,12 @@ def add(x, y):
 
 def multiple(x,y):
     return x * y
+
+def cmd_add(args=None):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x', type=float)
+    parser.add_argument('y', type=float)
+    parsed_args = parser.parse_args(args)
+
+    print(add(parsed_args.x, parsed_args.y))
